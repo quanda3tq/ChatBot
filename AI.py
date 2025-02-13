f5501395248999b40baec50274d659b0e202f941
quanda3tq <quanda3.tq@gmail.com>
2025-02-13 22:42:23 +0700

    add choose voice or enter by keyboard

--- AI.py
+++ AI.py
@@ -3,62 +3,85 @@ import pyttsx3
 import wikipedia
 from datetime import date, datetime
 
+# Khởi tạo giọng nói
 Robot_mouth = pyttsx3.init()
 voices = Robot_mouth.getProperty('voices') 
-Robot_mouth.setProperty('voice', voices[1].id)   #Changes voices. 1 for female, 0 for male
+Robot_mouth.setProperty('voice', voices[1].id)  # Chọn giọng nữ (1), giọng nam là (0)
 Robot_mouth.setProperty('rate', 200)
 
-Robot_ear = speech_recognition.Recognizer() 
+# Khởi tạo nhận diện giọng nói
+Robot_ear = speech_recognition.Recognizer()
 Robot_brain = ""
 
+# Chọn phương thức nhập một lần duy nhất
 while True:
-    #Nghe
-    with speech_recognition.Microphone(device_index=None) as mic: #4: Mic loa ngoai laptop 
-        print("Robot: I'm listening")
-        audio = Robot_ear.listen(mic)
-        
-    print("Robot: ...")
+    Robot_brain = "Choose input method: 1 is Voice, 2 is Keyboard"
+    print("\nChoose input method: (1) Voice, (2) Keyboard")
+    choice = input("Enter 1 or 2: ").strip()
 
-    try:
-        you = Robot_ear.recognize_google(audio)
-    except:
-        you = ""
-    print("you: " + you)
+    if choice in ["1", "2"]:
+        break  # Thoát khỏi vòng lặp khi chọn đúng
+    else:
+        print("Invalid choice! Please enter 1 or 2.")
+
+while True:
+    if choice == "1":
+        # Nhận dữ liệu từ microphone
+        with speech_recognition.Microphone(device_index=None) as mic:  
+            print("Robot: I'm listening...")
+            audio = Robot_ear.listen(mic)
+
+        print("Robot: Processing...")
+
+        try:
+            you = Robot_ear.recognize_google(audio)
+        except:
+            you = ""
+    else:
+        # Nhập từ bàn phím
+        you = input("You: ").strip()
+
+    print("You: " + you)
+
+    # Xử lý câu lệnh
+    your_lower = you.lower()
     
-    #Hieu
-    if "hello" in you:
+    if "hello" in your_lower:
         Robot_brain = "Hello DFox"
-    elif "today" in you:
+    elif "today" in your_lower:
         today = date.today()
         Robot_brain = today.strftime("%B %d, %Y")
-    elif "time" in you:
+    elif "time" in your_lower:
         now = datetime.now()
-        Robot_brain = now.strftime("%H hourse %M")
-    elif "introduce yourself" in you:
-        Robot_brain = "I'm a Chatbot make by DF"
-    elif "thank" in you:
-        Robot_brain = "you're welcom"
-
-    elif "search" in you or "who is" in you or "What about" in you: #Wikipedia
+        Robot_brain = now.strftime("%H hours %M minutes")
+    elif "bye" in your_lower:
+        Robot_brain = "See you again"
+        print("Robot: " + Robot_brain)
+        Robot_mouth.say(Robot_brain)
+        Robot_mouth.runAndWait()
+        break  # Thoát chương trình
+    elif "who am i" in your_lower:
+        Robot_brain = "Maybe you are my boss, a good person"
+    elif "introduce yourself" in your_lower:
+        Robot_brain = "I'm a Chatbot made by DF"
+    elif "thank" in your_lower:
+        Robot_brain = "You're welcome"
+    elif "search" in your_lower or "who is" in your_lower or "What about" in your_lower:
         try:
-            query = you.replace("search", "").replace("who is", "").strip()
-            Robot_brain = wikipedia.summary(query, sentences=2)  # Lấy 2 câu đầu tiên từ Wikipedia
-        except wikipedia.exceptions.DisambiguationError as e:
+            query = your_lower.replace("search", "").replace("who is", "").replace("What about", "").strip()
+            if query:
+                Robot_brain = wikipedia.summary(query, sentences=2)  # Lấy 2 câu đầu tiên từ Wikipedia
+            else:
+                Robot_brain = "Please provide a topic to search on Wikipedia."
+        except wikipedia.exceptions.DisambiguationError:
             Robot_brain = "There are multiple results. Please be more specific."
         except wikipedia.exceptions.PageError:
             Robot_brain = "I couldn't find anything on Wikipedia about that."
-    
-    elif "bye" in you: #Goobye, Dừng chatbot
-        Robot_brain = "See you again"
-        #Noi
-        Robot_mouth.say(Robot_brain)
-        Robot_mouth.runAndWait()
-        print("Robot: " + Robot_brain)
-        break
     else:
         Robot_brain = "I don't understand. Please say again."
 
     print("Robot: " + Robot_brain)
-#Noi
+
+    # Nói
     Robot_mouth.say(Robot_brain)
     Robot_mouth.runAndWait()
