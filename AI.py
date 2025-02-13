ffc2f1ded6ab370c5d40ccf04f60dac85ec0a6f6
quanda3tq <quanda3.tq@gmail.com>
2025-02-13 22:14:32 +0700

    add wikipedia_search

--- AI.py
+++ AI.py
@@ -1,10 +1,11 @@
 import speech_recognition
 import pyttsx3
+import wikipedia
 from datetime import date, datetime
 
 Robot_mouth = pyttsx3.init()
 voices = Robot_mouth.getProperty('voices') 
-Robot_mouth.setProperty('voice', voices[2].id)   #Changes voices. 1 for female, 0 for male
+Robot_mouth.setProperty('voice', voices[1].id)   #Changes voices. 1 for female, 0 for male
 Robot_mouth.setProperty('rate', 200)
 
 Robot_ear = speech_recognition.Recognizer() 
@@ -12,7 +13,7 @@ Robot_brain = ""
 
 while True:
     #Nghe
-    with speech_recognition.Microphone(device_index=4) as mic:
+    with speech_recognition.Microphone(device_index=None) as mic: #4: Mic loa ngoai laptop 
         print("Robot: I'm listening")
         audio = Robot_ear.listen(mic)
         
@@ -33,21 +34,29 @@ while True:
     elif "time" in you:
         now = datetime.now()
         Robot_brain = now.strftime("%H hourse %M")
-    elif "bye" in you:
+    elif "introduce yourself" in you:
+        Robot_brain = "I'm a Chatbot make by DF"
+    elif "thank" in you:
+        Robot_brain = "you're welcom"
+
+    elif "search" in you or "who is" in you or "What about" in you: #Wikipedia
+        try:
+            query = you.replace("search", "").replace("who is", "").strip()
+            Robot_brain = wikipedia.summary(query, sentences=2)  # Lấy 2 câu đầu tiên từ Wikipedia
+        except wikipedia.exceptions.DisambiguationError as e:
+            Robot_brain = "There are multiple results. Please be more specific."
+        except wikipedia.exceptions.PageError:
+            Robot_brain = "I couldn't find anything on Wikipedia about that."
+    
+    elif "bye" in you: #Goobye, Dừng chatbot
         Robot_brain = "See you again"
         #Noi
         Robot_mouth.say(Robot_brain)
         Robot_mouth.runAndWait()
         print("Robot: " + Robot_brain)
         break
-    elif "who am I" in you:
-        Robot_brain = "maybe you are my boss, a good boy"
-    elif "introduce yourself" in you:
-        Robot_brain = "I'm a Chatbot make by DF"
-    elif "thank" in you:
-        Robot_brain = "you're welcom"
     else:
-        Robot_brain = "again"
+        Robot_brain = "I don't understand. Please say again."
 
     print("Robot: " + Robot_brain)
 #Noi
